b389514d2aa894a9d64e869dc098f52b6325a8eb
Jake Ginnivan <jake@ginnivan.net>
2017-01-24 11:39:28 +0800

    Use test fixture in ssr tests

--- tests/data-loader.ssr.spec.tsx
+++ tests/data-loader.ssr.spec.tsx
@@ -5,16 +5,7 @@ import { Provider } from 'react-redux'
 import { OwnProps, LoadedState, createTypedDataLoader } from '../src/data-loader'
 import { ReduxStoreState, reducer } from '../src/data-loader.redux'
 import PromiseCompletionSource from './helpers/promise-completion-source'
-
-interface Data {
-    result: string
-}
-
-const TestDataLoader = createTypedDataLoader<Data>()
-
-const Verifier: React.SFC<LoadedState<Data> & {
-    renderCount: number
-}> = (loadedState) => (<noscript />)
+import ComponentFixture, { Verifier } from './helpers/component-fixture'
 
 let store: Store<ReduxStoreState>
 
@@ -23,89 +14,60 @@ beforeEach(() => {
     store = createStore(combineReducers<ReduxStoreState>({ dataLoader: reducer }))
 })
 
-let loadDataCount: number
-let testDataPromise: PromiseCompletionSource<Data>
-
-const mountComponent = (isServerSideRender: boolean, resetCounters = true) => {
-    testDataPromise = new PromiseCompletionSource<Data>()
-    let renderCount = 0
-    if (resetCounters) {
-        loadDataCount = 0
-    }
-    const testComponent = (
-        <Provider store={store}>
-            <TestDataLoader
-                dataType="testDataType"
-                dataKey="testKey"
-                isServerSideRender={isServerSideRender}
-                loadData={() => {
-                    loadDataCount++
-                    return testDataPromise.promise
-                }}
-                renderData={(props) => (
-                    <Verifier {...props} renderCount={++renderCount} />
-                )}
-            />
-        </Provider>
-    )
-
-    return mount(testComponent).find(TestDataLoader)
-}
-
 describe('server side render', () => {
     it('should start loading data if not loaded', () => {
-        const sut = mountComponent(true)
+        const sut = new ComponentFixture(store, true)
 
-        const verifier = sut.find(Verifier)
+        const verifier = sut.component.find(Verifier)
 
         expect(verifier.props()).toMatchSnapshot()
         expect(store.getState()).toMatchSnapshot()
-        expect(loadDataCount).toBe(1)
+        expect(sut.loadDataCount).toBe(1)
     })
 
     it('should pass loaded data once promise resolves', async() => {
-        const sut = mountComponent(true)
+        const sut = new ComponentFixture(store, true)
 
-        const verifier = sut.find(Verifier)
+        const verifier = sut.component.find(Verifier)
 
-        await testDataPromise.resolve({
+        await sut.testDataPromise.resolve({
             result: 'Success!'
         })
 
         expect(verifier.props()).toMatchSnapshot()
         expect(store.getState()).toMatchSnapshot()
-        expect(loadDataCount).toBe(1)
+        expect(sut.loadDataCount).toBe(1)
     })
 
     it('should pass failure when data load fails', async() => {
-        const sut = mountComponent(true)
+        const sut = new ComponentFixture(store, true)
 
-        const verifier = sut.find(Verifier)
+        const verifier = sut.component.find(Verifier)
 
-        await testDataPromise.reject(new Error('Boom!'))
+        await sut.testDataPromise.reject(new Error('Boom!'))
 
         expect(verifier.props()).toMatchSnapshot()
         expect(store.getState()).toMatchSnapshot()
-        expect(loadDataCount).toBe(1)
+        expect(sut.loadDataCount).toBe(1)
     })
 
     it('second SSR when data loaded should not reload data', async() => {
-        let sut = mountComponent(true)
-        const verifier = sut.find(Verifier)
-        await testDataPromise.resolve({ result: 'Success!' })
+        let sut = new ComponentFixture(store, true)
+        const verifier = sut.component.find(Verifier)
+        await sut.testDataPromise.resolve({ result: 'Success!' })
 
-        sut = mountComponent(true, false)
+        sut = new ComponentFixture(store, true)
 
-        expect(loadDataCount).toBe(1)
+        expect(sut.loadDataCount).toBe(0)
     })
 
     it('second SSR when data load failed should not reload data', async() => {
-        let sut = mountComponent(true)
-        const verifier = sut.find(Verifier)
-        await testDataPromise.reject(new Error('Boom'))
+        let sut = new ComponentFixture(store, true)
+        const verifier = sut.component.find(Verifier)
+        await sut.testDataPromise.reject(new Error('Boom'))
 
-        sut = mountComponent(true, false)
+        sut = new ComponentFixture(store, true)
 
-        expect(loadDataCount).toBe(1)
+        expect(sut.loadDataCount).toBe(0)
     })
 })
