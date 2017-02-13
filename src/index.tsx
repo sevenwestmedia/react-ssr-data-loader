9a53ace77cde7de48dc6b36a771f337ed351a001
Jake Ginnivan <jake.ginnivan@wanews.com.au>
2017-02-14 06:50:40 +0800

    Export a few more things from the index

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 export { reducer, ReduxStoreState } from './data-loader.redux'
-export { createTypedDataLoader } from './data-loader'
+export { createTypedDataLoader, RenderData, LoadedState as RenderProps } from './data-loader'
 export { default as DataProvider } from './data-provider'
 export { default as IsLoading } from './is-loading'
